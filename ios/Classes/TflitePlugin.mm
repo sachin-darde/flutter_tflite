5d85ccbb5cf05ad829ea700210e3ca2bebf767a5
sachin-darde <98461588+sachin-darde@users.noreply.github.com>
2022-06-05 16:28:53 +0530

    Update TflitePlugin.mm

--- ios/Classes/TflitePlugin.mm
+++ ios/Classes/TflitePlugin.mm
@@ -26,7 +26,17 @@
 #include "tensorflow/lite/op_resolver.h"
 #endif
 
-#include "ios_image_load.h"
+#include <vector>
+
+std::vector<uint8_t> LoadImageFromFile(const char* file_name,
+						 int* out_width,
+						 int* out_height,
+						 int* out_channels);
+
+NSData *CompressImage(NSMutableData*,
+						 int width,
+						 int height,
+             int bytesPerPixel);
 
 #define LOG(x) std::cerr
 
